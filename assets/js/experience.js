d9abf6ffa6391a0449ac950418a65f0f1d6db63d
Santiago Fisela <71735491+PinkLittleKitty@users.noreply.github.com>
2023-06-08 07:48:13 -0300

    Actualizar experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -23,7 +23,7 @@ const exp = [
     cardImage: "assets/images/experience-page/Logo3.png",
     place: "Juego en desarrollo.",
     time: "(Mayo, 2022 - Actualidad)",
-    desp: "<li>Juego con estética voxel de gestión de recursos.</li> <li>Aproximadamente 8 personas trabajando en él, siendo yo el desarrollador principal (Y único, al momento de escribir esto).</li>",
+    desp: "<li>Juego con estética voxel de gestión de recursos.</li> <li>Aproximadamente 8 personas trabajando en él, siendo yo el programador jefe de tres.</li>",
   },
 ];
 
